72d795c363b8d0872ee52d6a80d3d976d7c294d5
Pham Quang <quangpv598@gmail.com>
2022-03-30 03:03:06 +0700

    # add email service

--- HanbiroExtensionGUI/Services/EmailService.cs
+++ HanbiroExtensionGUI/Services/EmailService.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using HanbiroExtensionGUI.Resources;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+using MimeKit.Utils;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HanbiroExtensionGUI.Services
 {
-    class EmailService
+    public interface IEmailService
     {
+        Task Send(string from, string to, string subject, string message, string imagePath);
+    }
+
+    public class EmailService : IEmailService
+    {
+        public EmailService()
+        {
+            
+        }
+
+        public async Task Send(string from, string to, string subject, string message, string imagePath)
+        {
+            // create message
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(from));
+            email.To.Add(MailboxAddress.Parse(to));
+            email.Subject = subject;
+
+            var builder = new BodyBuilder();
+            var image = builder.LinkedResources.Add(imagePath);
+
+            image.ContentId = MimeUtils.GenerateMessageId();
+
+            builder.HtmlBody = string.Format(@"<p>{0}!</p><img src=""cid:{1}"">", message, image.ContentId);
+
+
+            email.Body = builder.ToMessageBody();
+
+            // send email
+            using var smtp = new SmtpClient();
+            await smtp.ConnectAsync(AppSettings.SmtpHost, 587, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(AppSettings.SmtpUser, AppSettings.SmtpPass);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
     }
 }
--- HanbiroExtensionGUI/frmMain.cs
+++ HanbiroExtensionGUI/frmMain.cs
@@ -33,6 +33,12 @@ namespace HanbiroExtensionGUI
         {
             InitializeComponent();
             LoadUserSettings();
+            EmailService emailService = new EmailService();
+            emailService.Send("vanquang598@gmail.com",
+                "quangpv598@gmail.com",
+                "Xin chào",
+                "Thời gian login",
+                @"C:\Users\quang\Downloads\z3293085024869_1c5d2ba3bd26c6c66b41af40e2d40621.jpg");
         }
         #endregion
 
