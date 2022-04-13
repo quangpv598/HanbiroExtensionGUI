75b4d8aecba7e91a429d7e3cc265c2ff7d50e09f
Pham Quang <quangpv598>
2022-04-13 12:58:03 +0700

    update standard version

---
+++ HanbiroExtensionApp/HanbiroRequestHandler.cs
@@ -0,0 +1,37 @@
+﻿using CefSharp;
+using CefSharp.Handler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanbiroExtensionApp
+{
+    class CustomResourceRequestHandler : ResourceRequestHandler
+    {
+        public CustomResourceRequestHandler()
+        {
+            
+        }
+
+        protected override void OnResourceLoadComplete(IWebBrowser browserControl, IBrowser browser, IFrame frame, IRequest request, IResponse response, UrlRequestStatus status, long receivedContentLength)
+        {
+            
+        }
+    }
+
+
+    public class HanbiroRequestHandler : RequestHandler
+    {
+        public HanbiroRequestHandler()
+        {
+            
+        }
+        protected override IResourceRequestHandler GetResourceRequestHandler(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IRequest request, bool isNavigation, bool isDownload, string requestInitiator, ref bool disableDefaultHandling)
+        {
+            return new CustomResourceRequestHandler();
+        }
+
+    }
+}
