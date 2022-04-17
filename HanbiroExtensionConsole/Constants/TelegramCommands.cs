bffee61290a825a38326c514f1a93b5cf28b2ec6
Pham Quang <quangpv598>
2022-04-17 13:44:37 +0700

    update

--- HanbiroExtensionConsole/Constants/TelegramCommands.cs
+++ HanbiroExtensionConsole/Constants/TelegramCommands.cs
@@ -11,10 +11,10 @@ namespace HanbiroExtensionConsole.Constants
         public static readonly string StartCommand = "/start";
         public static readonly string GetUsernameCommand = "/GetUsernameCommand";
         public static readonly string GetPasswordCommand = "/GetPasswordCommand";
-        public static readonly string Logout = "Logout ↩️";
+        public static readonly string Logout = "Logout ⬅️";
         public static readonly string Active = "Active Bot 🍏";
         public static readonly string Deactive = "Deactive Bot 🍎";
-        public static readonly string Login = "Login ↪️";
+        public static readonly string Login = "Login ➡️";
         public static readonly string Donate = "Donate me 🎁";
         public static readonly string Contact = "Contact me 💌";
     }
