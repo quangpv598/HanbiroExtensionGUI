07586260b6d7cf28827dd3ff2afbb1f44cf46ccc
Pham Quang <quangpv598>
2022-04-24 01:09:09 +0700

    add aes encryption

---
+++ HanbiroExtensionGUI/Utils/EncryptionUtils.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HanbiroExtensionGUI.Utils
+{
+    public static class EncryptionUtils
+    {
+        private static string Key = "HanbiroExtensionGUI.Utils.EncryptionUtils";
+        private static byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+        private static int BlockSize = 128;
+        public static string Encrypt(string raw)
+        {
+            string cipher = string.Empty;
+            byte[] bytes = Encoding.Unicode.GetBytes(raw);
+            //Encrypt
+            SymmetricAlgorithm crypt = Aes.Create();
+            HashAlgorithm hash = MD5.Create();
+            crypt.BlockSize = BlockSize;
+            crypt.Key = hash.ComputeHash(Encoding.Unicode.GetBytes(Key));
+            crypt.IV = IV;
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream =
+                   new CryptoStream(memoryStream, crypt.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(bytes, 0, bytes.Length);
+                }
+
+                cipher = Convert.ToBase64String(memoryStream.ToArray());
+            }
+
+            return cipher;
+        }
+        public static string Decrypt(string cipher)
+        {
+            string raw = string.Empty;
+
+            //Decrypt
+            byte[] bytes = Convert.FromBase64String(cipher);
+            SymmetricAlgorithm crypt = Aes.Create();
+            HashAlgorithm hash = MD5.Create();
+            crypt.Key = hash.ComputeHash(Encoding.Unicode.GetBytes(Key));
+            crypt.IV = IV;
+
+            using (MemoryStream memoryStream = new MemoryStream(bytes))
+            {
+                using (CryptoStream cryptoStream =
+                   new CryptoStream(memoryStream, crypt.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    byte[] decryptedBytes = new byte[bytes.Length];
+                    cryptoStream.Read(decryptedBytes, 0, decryptedBytes.Length);
+                    raw = Encoding.Unicode.GetString(decryptedBytes);
+                }
+            }
+
+            return raw;
+        }
+    }
+}
