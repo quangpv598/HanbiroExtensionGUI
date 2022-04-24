6e1aab8bbeba75abf24c1bfe5da48a6dbed9de5c
Pham Quang <quangpv598>
2022-04-25 00:20:57 +0700

    update time processing

--- HanbiroExtensionGUI/Models/AppSettings.cs
+++ HanbiroExtensionGUI/Models/AppSettings.cs
@@ -15,7 +15,7 @@ namespace HanbiroExtensionGUI.Models
             get
             {
                 return DateTime.Now.TimeOfDay <= TimeWork.StartTime.TimeOfDay.Add(TimeSpan.FromMinutes(15))
-                    && DateTime.Now.TimeOfDay >= TimeWork.StartTime.TimeOfDay.Add(TimeSpan.FromMinutes(-5));
+                    && DateTime.Now.TimeOfDay >= TimeWork.StartTime.TimeOfDay.Add(TimeSpan.FromMinutes(-15));
             }
         }
         public bool IsClockingOut
@@ -23,7 +23,7 @@ namespace HanbiroExtensionGUI.Models
             get
             {
                 return DateTime.Now.TimeOfDay <= TimeWork.EndTime.TimeOfDay.Add(TimeSpan.FromMinutes(15))
-                    && DateTime.Now.TimeOfDay >= TimeWork.StartTime.TimeOfDay.Add(TimeSpan.FromMinutes(-5));
+                    && DateTime.Now.TimeOfDay >= TimeWork.StartTime.TimeOfDay.Add(TimeSpan.FromMinutes(-15));
             }
         }
         public List<long> Adminitrators { get; set; }
