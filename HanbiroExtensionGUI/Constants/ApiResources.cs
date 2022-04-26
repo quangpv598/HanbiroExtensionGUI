47507abe1c1f779fbff8ca8d33eae9c16cbe15e9
Pham Quang <quangpv598>
2022-04-26 13:01:42 +0700

    update resouces api

--- HanbiroExtensionGUI/Constants/ApiResources.cs
+++ HanbiroExtensionGUI/Constants/ApiResources.cs
@@ -11,7 +11,7 @@ namespace HanbiroExtensionGUI.Constants
         public static readonly string LoginSignal = "/ngw/app/lib/css/input-password.css";
         public static readonly string Auth = "/ngw/sign/auth";
         public static readonly string GetUserInfoSignal = "/ngw/common/config/mode/all?im_not_app=1";
-        public static readonly string AuthSuccessSignal = "/ngw/app/template/main/dashboard.html";
+        public static readonly string AuthSuccessSignal = "/ngw/main/header_link";//"/ngw/app/template/main/dashboard.html";
         public static readonly string ClockIn = "/ngw/timecard/punch_v2/in";
         public static readonly string ClockOut = "/ngw/timecard/punch_v2/out";
         public static readonly string GetTimeCard = "/ngw/timecard/punch_v2/time";
